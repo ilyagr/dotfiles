c39cdcb9c27ce99592642400998654bb36933e05
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2021-02-08 17:24:54 -0800

    History

--- config_scripts/bash_config.bash
+++ config_scripts/bash_config.bash
@@ -1,3 +1,13 @@
 export EDITOR=vim
 # Debian standard prompt from ~/.bashrc + \n
-PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\] \[\033[01;34m\]\w\[\033[00m\]\n\$ '
+PS1='${debian_chroot:+($debian_chroot)}[\@] \[\033[01;32m\]\h\[\033[00m\] \[\033[01;34m\]\w\[\033[00m\]\n\$ '
+
+# Clunky, but least buggy option available
+HISTCONTROL=ignoreboth:erasedups
+shopt -s histappend
+PS0='`history -a && history -c && history -r`'$PS0
+
+shopt -s histverify
+HISTTIMEFORMAT="%r "
+HISTFILESIZE=50000
+HISTSIZE=60000
