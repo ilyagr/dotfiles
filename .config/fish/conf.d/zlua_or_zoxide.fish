de31d12a9dc110db43b060c69841188d44edfe84
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2022-05-20 13:31:12 -0700

    Minor fish

--- .config/fish/conf.d/zlua_or_zoxide.fish
+++ .config/fish/conf.d/zlua_or_zoxide.fish
@@ -1,5 +1,5 @@
 set zlua_path ~/.local/app/z.lua
-if  which lua > /dev/null && test -r $zlua_path
+if which lua > /dev/null && test -r $zlua_path
     lua $zlua_path --init fish once fzf | source
 
     # It uses `builtin cd by default, which messes up `cdh`.
@@ -27,4 +27,12 @@ if  which lua > /dev/null && test -r $zlua_path
 
 else if which zoxide > /dev/null
   zoxide init fish | source
+else
+  function z
+      echo 'For `z` to work, either '
+      echo ' 1) Install `lua` and download z.lua to ' $zlua_path
+      echo ' 2) Install `zoxide` to somewhere in the PATH'
+      return 1
+  end
+  alias zi=z
 end
