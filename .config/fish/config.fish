0570b1a02ad9b9dba19d44c241d3232def73c340
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2023-07-07 20:28:10 -0700

    fish: move `fish_prompt_pwd_dir_length` to config.fish
    This was previously set via `set -U`, and universal variables are very
    annoying.

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Note that nonstandard implementations of `prompt_pwd` tend to not respect this
+set -g fish_prompt_pwd_dir_length 10
+
