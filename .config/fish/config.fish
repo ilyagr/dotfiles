be1177c3ff9ab04a6edd75b2fb37864ccdc74b7b
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2023-07-08 21:50:46 -0700

    ripgrep: allow automatic use of PCRE for lookahead

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,5 +4,5 @@ end
 
 # Note that nonstandard implementations of `prompt_pwd` tend to not respect this
 set -g fish_prompt_pwd_dir_length 10
-set -g fish_greeting
+set -g fish_greeting "Welcome to fish, the friendly interactive shell."
 
