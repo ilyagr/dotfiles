b3e7de5b1be3a01645b48a354d867feabee5274b
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2024-10-11 22:58:09 -0700

    abbr for :/

--- .config/fish/conf.d/abbrs_and_aliases.fish
+++ .config/fish/conf.d/abbrs_and_aliases.fish
@@ -1,3 +1,6 @@
+alias lf lfcd
+alias ranger ranger-cd
+
 abbr -a -g -- ri 'git rebase --autostash --autosquash --interactive HEAD~5'
 # See --fork-point in `git help merge-base`. Apparently, it's a bit weird
 # https://public-inbox.org/git/xmqqbmivyih1.fsf@gitster.mtv.corp.google.com/t/#rbe417781d60bd429a40584a00c450bed4946fde1
@@ -18,5 +21,11 @@ abbr -a -g -- vs 'EDITOR="code --wait"'
 # jj
 abbr -a -g -- vsd 'EDITOR="code --wait" jj describe'
 
-alias lf lfcd
-alias ranger ranger-cd
+function _replace_gitdir
+    set -l dir (git rev-parse --path-format=relative --show-toplevel 2>/dev/null || return 1)
+    # set -l dir (git rev-parse --show-cdup 2>/dev/null || return 1)
+    string replace -r "^:/" "$dir" $argv
+end
+abbr ':/' --position anywhere --regex '^:/.*' --function _replace_gitdir
+
+bind tab expand-abbr complete  # ?!
