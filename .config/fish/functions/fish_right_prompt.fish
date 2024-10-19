d738454352e9a3e441fd5c2775bdb5d6de96ff5f
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2024-10-18 20:14:30 -0700

    fish: fancier command duration into right prompt

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -5,13 +5,34 @@ function fish_right_prompt
     end
 
     set -l duration "$cmd_duration$CMD_DURATION"
+    set -l output ""
+    set -l numberfmt (set_color green)
+    set -l unitfmt  (set_color normal)
+
     # See https://github.com/pure-fish/pure/blob/28447d2e7a4edf3c954003eda929cde31d3621d2/functions/_pure_format_time.fish#L4
     # for fancier version
-    if test $duration -gt 500
-        set duration (math -s 2 $duration / 1000)s
-        printf '⏲%s' (set_color green)$duration(set_color normal)
-    # else
-    #     set duration
+
+    set -l minute (math "1000 * 60")
+    set -l hour (math "$minute * 60")
+    set -l day  (math "$hour * 24")
+    if test $duration -gt $day
+        set output (printf "%s"$numberfmt"%s"$unitfmt"d" $output (math -s 0 $duration / $day) )
+        set duration "$duration % $day"
+    end
+    if test $duration -gt $hour
+        set output (printf "%s"$numberfmt"%s"$unitfmt"h" $output (math -s 0 $duration / $hour) )
+        set duration "$duration % $hour"
+    end
+    if test $duration -gt $minute
+        set output (printf "%s"$numberfmt"%s"$unitfmt"m" $output (math -s 0 $duration / $minute) )
+        set duration "$duration % $minute"
+    end
+    if test -n "$output" || test $duration -gt 500
+        set output (printf "%s"$numberfmt"%s"$unitfmt"s" $output (math -s 2 $duration / 1000))
+    end
+
+    if test -n "$output"
+        printf '⏲%s' $output(set_color normal)
     end
 
     if type -q tput
