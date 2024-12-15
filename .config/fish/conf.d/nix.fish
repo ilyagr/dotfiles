c6c34059e91200bac07023bc440b51b3ac61413c
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2024-12-14 20:42:56 -0800

    nix.fish: try to not execute it too many times

--- .config/fish/conf.d/nix.fish
+++ .config/fish/conf.d/nix.fish
@@ -1,15 +1,19 @@
-if test -e $HOME/.nix-profile/etc/profile.d/nix.fish
-    . $HOME/.nix-profile/etc/profile.d/nix.fish
-    set -ax MANPATH (manpath -g)
+#if not string match -q  '*/nix/var/nix/profiles/default/share*' "$XDG_DATA_DIRS"
+if test -n "$NIX_PROFILES"
+    # Try to only run this once.
+    if test -e $HOME/.nix-profile/etc/profile.d/nix.fish
+        . $HOME/.nix-profile/etc/profile.d/nix.fish
+        set -ax MANPATH (manpath -g)
 
-    # Avoids weird problems with `nix develop` running `bash` inside `bash`
-    # ... but creates weirder problems on mac os, where /usr/bin/bash is old.
-    # alias bash /usr/bin/bash
-end # added by Nix installer
+        # Avoids weird problems with `nix develop` running `bash` inside `bash`
+        # ... but creates weirder problems on mac os, where /usr/bin/bash is old.
+        # alias bash /usr/bin/bash
+    end # added by Nix installer
 
-if test -e /nix/var/nix/profiles/default/etc/profile.d/nix.fish
-    . /nix/var/nix/profiles/default/etc/profile.d/nix.fish
-    set -ax MANPATH (manpath -g)
+    if test -e /nix/var/nix/profiles/default/etc/profile.d/nix.fish
+        . /nix/var/nix/profiles/default/etc/profile.d/nix.fish
+        set -ax MANPATH (manpath -g)
+    end
 end
 
 if command -q nix-your-shell
