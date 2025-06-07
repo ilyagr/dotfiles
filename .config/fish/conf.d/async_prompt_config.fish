a4112c3849bd9599903ee7eb0e67b35bb473ea49
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2025-06-07 15:16:41 -0700

    fish-async-prompt config using my custom stuff

--- .config/fish/conf.d/async_prompt_config.fish
+++ .config/fish/conf.d/async_prompt_config.fish
@@ -1,3 +1,11 @@
+if test "$TERM_PROGRAM" = vscode
+    set -g async_prompt_enable 0
+    return
+end
+
+# The first of these variable definitions currently doesn't work, because
+# it's loaded after __async_prompt.fish The second may or may not work,
+# TODO to find out
 set -g async_prompt_on_variable fish_bind_mode PWD __ilyagr_jj_op
 set -g async_prompt_config_functions fish_prompt fish_right_prompt fish_git_prompt fish_jj_prompt
 
